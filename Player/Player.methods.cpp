38648b8949d5e6613297ef1af5e133ea6115ba9c
Balthazar Gronon <bgronon@gmail.com>
2015-01-10 21:22:26 +0100

    fix(player): maxs

--- Player/Player.methods.cpp
+++ Player/Player.methods.cpp
@@ -14,6 +14,21 @@
 
 void Player::move (float const x, float const y)
 {
+  if (x < 0) {
+    this->_x = 0;
+    return ;
+  }
+  if (y < 0) {
+    this->_y = 0;
+    return ;
+  }
+  if (x + this->_height >= MAX_HEIGHT) {
+    return ;
+  }
+  if (y + this->_width >= MAX_WIDTH) {
+    return ;
+  }
+
   this->_x = x;
   this->_y = y;
 }
