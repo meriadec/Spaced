1453075298744273dbff92012a2c76374b7a0337
meriadec <meriadec.pillet@gmail.com>
2015-01-11 17:52:45 +0100

    refactor(level): move sprites and rearrange level declaration

--- Game/Game.methods.cpp
+++ Game/Game.methods.cpp
@@ -58,8 +58,18 @@ void Game::destroy (void)
 
 void Game::loop (void)
 {
-  std::string level1_sprites[] = { "enemy1", "enemy2", "enemy3" };
+
+  // Level 1
+  // -------
+
+  std::string level1_sprites[] = {
+    "lv1/enemy1",
+    "lv1/enemy2",
+    "lv1/enemy3"
+  };
+
   Level level1("Level 1 - Friendly fire", 0.008, 10, 3, level1_sprites);
   level1.start();
   level1.loop();
+
 }
